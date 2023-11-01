2f82f536606e4f26bc18b646f5a72caf9834c0aa
joaoxyz <jpreis808@gmail.com>
2023-11-01 15:59:39 -0300

    Add search and filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css';
+
+import Filter from './components/Filter/Filter';
+import Search from './components/Search/Search';
 import Todo from './components/Todo/Todo';
 import TodoForm from './components/TodoForm/TodoForm';
 
@@ -24,6 +27,11 @@ function App() {
             isCompleted: false
         }
     ])
+
+    const [search, setSearch] = useState("")
+
+    const [filter, setFilter] = useState("All")
+    const [sort, setSort] = useState("Asc")
 
     const addTodo = (text, category) => {
         const newTodos = [...todos, {
@@ -55,8 +63,10 @@ function App() {
     return (
         <div className="app">
             <h1>Lista de Tarefas</h1>
+            <Search search={search} setSearch={setSearch}/>
+            <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
             <div className='todo-list'>
-                {todos.map((todo) => (
+                {todos.filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted).filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text) ).map((todo) => (
                     <Todo todo={todo} key={todo.id} removeTodo={removeTodo} completeTodo={completeTodo} />
                 ))}
             </div>
